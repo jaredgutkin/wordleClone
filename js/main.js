8aa898487b0e7c18d00dc9552f4919f9d12c7197
Jared Gutkin <jgutkinsam@gmail.com>
2022-04-10 12:28:48 -0500

    press key completly done

--- js/main.js
+++ js/main.js
@@ -15289,7 +15289,7 @@ const dictionary = [
     "rural",
     "shave"
   ]
-
+const WORD_LENGTH = 5
 const guessGrid = document.querySelector("[data-guess-grid]")
 
 startInteraction()
@@ -15339,9 +15339,15 @@ function handleKeyPress(e) {
 }
 
 function pressKey(key) {
+    const activeTiles = getActiveTiles()
+    if (activeTiles.length >= WORD_LENGTH) return 
     const nextTile = guessGrid.querySelector(":not([data-letter])")
     nextTile.dataset.letter = key.toLowerCase()
     nextTile.textContent = key
     nextTile.dataset.state = "active"
+}
+
+function getActiveTiles() {
+    return guessGrid.querySelectorAll('[data-state="active"]')
 }
 
